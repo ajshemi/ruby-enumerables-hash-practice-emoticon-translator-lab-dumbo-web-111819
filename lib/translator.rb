707e84155f6cffc0ce3f3b2da0d15caf601b4230
essential-hacker-4489 <akpojotor@yahoo.com>
2019-11-11 20:04:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,41 @@
 # require modules here
-
 def load_library
-  # code goes here
+  #create hash with two keys :get_meaning and :get_emoticon
+  name={}
+  name[:get_meaning]={}
+  name[:get_emoticon]={}
+  #load_file and save to result
+  require "yaml"
+  result=YAML.load_file(filename)
+  
+  #create two hash and an array and populate them
+  hash_1={}
+  hash_2={}
+  keys=[]
+  result.each do |k,v|
+    hash_1[v[1]]={}
+    hash_2[v[0]]={}
+    keys << k
+  end
+  #populate the values of the :get_meaning and :get_emoticon keys
+  name[:get_meaning]=hash_1
+  name[:get_emoticon]=hash_2
+  
+  # the emoticon keys inside the 'get_meaning' hash point to their meanings
+  i=0
+  name[:get_meaning].map do |k|
+    name[:get_meaning][k[0]]=keys[i]
+  i+=1
+  end
+  
+  keys_1=name[:get_meaning].keys
+  # the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalent
+  j=0
+  name[:get_emoticon].map do |k|
+    name[:get_emoticon][k[0]]=keys_1[j]
+  j+=1
+  end
+  name
 end
 
 def get_japanese_emoticon
